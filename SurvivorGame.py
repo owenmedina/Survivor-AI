1ae35c69f028c24477473ae39a38b01e18d285da
Owen Medina <owensmedi@gmail.com>
2019-10-20 18:17:09 +0800

    Fix get_skill_levels method

--- SurvivorGame.py
+++ SurvivorGame.py
@@ -7,6 +7,7 @@ def main():
   print(constants.GAME_GREETING)
   print(constants.GAME_INSTRUCTIONS)
   print(constants.GAME_GOOD_LUCK)
+  print("\n")
 
   # initialize set of survivors as initially empty
   survivors = dict()
