de65335c54f17b83c20cd1d941a1e8ffc7599193
Owen Medina <owensmedi@gmail.com>
2019-10-21 12:54:00 +0800

    Change game rules  - make initial survivors = 3  - make skills 2  - remove constant win_condition

--- constants.py
+++ constants.py
@@ -1,13 +1,11 @@
-NUM_INITIAL_SURVIVORS = 6
-NUM_MAX_SURVIVORS_TO_RECRUIT = NUM_INITIAL_SURVIVORS
-NUM_SKILLS = 3
-WIN_CONDITION = 5
+NUM_INITIAL_SURVIVORS = 3
+NUM_SKILLS = 2
 # list of skills for survival
 skills = ["food", "shelter", "intellect"]
 GAME_GREETING = "Welcome to Survivor.....well kinda\n"
 GAME_SENTENCE1 = "This is a simplified, modified version of Survivor."
 GAME_SENTENCE2A = "In this game, you will be presented with " + str(NUM_INITIAL_SURVIVORS) + " initial survivors with varying levels of skills related to "
-GAME_SENTENCE2B = skills[0] + ", " + skills[1] + " and " + skills[2] + "."
+GAME_SENTENCE2B = skills[0] + " and " + skills[1] + "."
 GAME_SENTENCE2 = GAME_SENTENCE2A + GAME_SENTENCE2B
 GAME_SENTENCE3 = "Your goal will be to eliminate and recruit survivors to achieve a level of " + str(WIN_CONDITION) + " for all skills."
 GAME_INSTRUCTIONS = GAME_SENTENCE1 + "\n" + GAME_SENTENCE2 + "\n" + GAME_SENTENCE3 + "\n"
